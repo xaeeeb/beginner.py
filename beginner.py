2ac8f12353d1663b3636fcd30b3862423c33f0cb
xaeeeb <mdsaibrahman23@gmail.com>
2024-10-03 03:24:07 -0700

    Add files via upload

--- beginner.py
+++ beginner.py
@@ -1,23 +1,47 @@
-x = input("whats your name? ")
-
-x= x.strip()
-x=x.capitalize()   
-
-
-print(f"hello , {x}" )
-
-
-     
 
 
 
+#x= int(input("whats x? "))
+
+#y= int(input("whats y? "))
+
+
+#print(x+y)
+
+
+#x= float(input("whats x ? "))
+
+#y= float(input(" whats y? "))
+
+
+#z= round(x+y)
+
+#print(z)
+
+#def main():
+
+   # hello()
+   # name= input(" whats your name ? ")
+
+    #hello(name)
+
+
+#def hello(to=",please enter your name below"):
+   # print("hello,",to)
 
 
 
+#main()
 
 
+def main():
+    x=int(input("whats x? "))
+    print("x squared is ", square(x))
 
 
+def square(n):
+    return  n * n
+main()
 
 
 
