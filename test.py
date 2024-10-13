7b387dd827243ee2fce8b49d08122c532f25b33d
xaeeeb <mdsaibrahman23@gmail.com>
2024-10-13 01:37:14 -0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,42 @@
+
+
+
+
+
+
+#def main():
+   # test_square()
+
+
+#def test_square():
+   # assert square(2) ==4
+    #assert square(3) ==9
+
+
+#if __name__ == "__main__":
+    # main()
+
+
+
+from calculator import square
+
+
+
+def test_square(2):
+    assert square(2) == 4
+    assert square(3) == 9
+    assert square(0) == 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
