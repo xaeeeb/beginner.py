aa1e10a5ba8d89297d7571f4662516da0704edef
xaeeeb <mdsaibrahman23@gmail.com>
2024-10-15 00:36:52 -0700

    Add files via upload

---
+++ io.py
@@ -0,0 +1,43 @@
+
+
+
+
+students= input("whats your name? ")  
+
+with open("students.csv", "a") as file:
+    file.write(f"{students}\n")
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
